e06c7e55adbc4d3a57e82fccfad166b5332cafbb
Chantel Lowe <chantellowe17@gmail.com>
2019-08-02 03:42:03 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,17 +2,17 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  new_arr = []
+  #new_arr = []
   
   while i < collection.length
     new_arr << yield(collection[i])
     i += 1 
   end
   
-  if new_arr.any? {|i| i % 3 == 0 && i % 5 == 0 }
-    return new_arr.select[0] {|i| i % 3 == 0 && i % 5 == 0 }
-  else
-    return nil
-  end
+  #if new_arr.any? {|i| i % 3 == 0 && i % 5 == 0 }
+   # return new_arr.select[0] {|i| i % 3 == 0 && i % 5 == 0 }
+#  else
+ #   return nil
+  #end
   
 end
